3562d51f0cc9e9be70cc7080c265c7af9465c085
Your Name <you@example.com>
2024-10-02 11:36:43 +0900

    update first proejct

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -3,9 +3,19 @@
  */
 package org.example.app;
 
+import org.example.app.kafka.producer.SampleDataProducer;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        SampleDataProducer test = new SampleDataProducer(); 
+
+        try{
+            test.testKafka()
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- app/src/main/java/org/example/app/kafka/producer/SampleDataProducer.java
+++ app/src/main/java/org/example/app/kafka/producer/SampleDataProducer.java
@@ -8,6 +8,31 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import java.util.Properties;
 
 public class SampleDataProducer {
+
+    public testKafka() throws Exception {
+ 
+
+        // Get the generator type from the command-line arguments
+        // "StockSingle", "StockGroup", "StockSector", "StockTransaction", "PortfolioSingle"
+        String generatorType = "StockSingle";
+
+        // Create producer properties
+        Properties properties = new KafkaProperties().setSourceTopic("my-topic").setGroupId("my-group").build();
+
+        // Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a DataGenerator and generate and send data
+        SampleDataGenerator dataGenerator = DataGeneratorFactory.createDataGenerator(producer, generatorType);
+        dataGenerator.generateAndSend();
+
+        // Flush and close producer
+        producer.flush();
+        producer.close();
+    }
+
+
+
     public static void main(String[] args) throws Exception {
         if (args.length < 1) {
             throw new IllegalArgumentException("Please provide a generator type as an argument");
